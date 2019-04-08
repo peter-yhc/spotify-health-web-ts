e9fae86103157a45c027ce55cccd65413fa91a09
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-04-09 08:55:20 +1000

    Add eslint.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
 import Typography from '@material-ui/core/Typography';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
+import PropTypes from 'prop-types';
 
 const styles = {
   title: {
-    margin: '1em 0'
-  }
+    margin: '1em 0',
+  },
 };
 
 function App(props) {
-  const {classes} = props;
+  const { classes } = props;
   return (
     <div className="App">
       <header className="App-header">
-        <Typography className={classes.title} variant={'h2'}>Spotify Health Checks</Typography>
-        <Typography variant={'h5'}>
-          <Link href={'dud'}>
+        <Typography className={classes.title} variant="h2">Spotify Health Checks</Typography>
+        <Typography variant="h5">
+          <Link href="dud">
             New Session
           </Link>
         </Typography>
@@ -25,5 +26,9 @@ function App(props) {
     </div>
   );
 }
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+};
 
 export default withStyles(styles)(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
+import { mount } from 'enzyme';
 import React from 'react';
 import App from './App';
-import {mount} from 'enzyme';
 
 test('render title', () => {
-  const title = mount(<App/>).find('h2');
+  const title = mount(<App />).find('h2');
   expect(title).toHaveLength(1);
 });
 
 test('render link', () => {
-  const link = mount(<App/>).find('a');
+  const link = mount(<App />).find('a');
   expect(link).toHaveLength(1);
 });
