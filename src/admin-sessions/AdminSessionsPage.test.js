ddabf82418ee94f3fbac69c624af6dc88f170cc6
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-05-03 13:02:09 +1000

    Remove unused import.

--- src/admin-sessions/AdminSessionsPage.test.js
+++ src/admin-sessions/AdminSessionsPage.test.js
@@ -1,4 +1,4 @@
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
