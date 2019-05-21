e00b72b102703c83f3aed8a39c4f6bd1eb7e435b
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-05-21 21:59:16 +1000

    Change ClientSessionPage test to use shallow testing.

--- src/client-sessions/ClientSessionPage.test.js
+++ src/client-sessions/ClientSessionPage.test.js
@@ -1,35 +1,23 @@
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
-import configureStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
 import { ClientSessionPage } from './ClientSessionPage';
 import HealthIndicatorCard from '../heath-indicators/HealthIndicatorCard';
 
-const mockStore = configureStore();
-const store = mockStore({
-  clientSessionReducer: { cards: [] },
-  debugPanelReducer: { history: [] },
-});
-
 describe('SessionsPage component', () => {
   test('render page', () => {
-    const wrapper = mount(
-      <Provider store={store}>
-        <ClientSessionPage classes={{ header: '', main: '' }} cards={[]} />,
-      </Provider>,
+    const wrapper = shallow(
+      <ClientSessionPage classes={{ header: '', main: '' }} cards={[]} />,
     );
     expect(wrapper.find('header').text()).toContain('Client Session');
     expect(wrapper.find(HealthIndicatorCard).length).toBe(0);
   });
 
   test('can render indicators if card present', () => {
-    const wrapper = mount(
-      <Provider store={store}>
-        <ClientSessionPage
-          classes={{ header: '', main: '' }}
-          cards={[{ indicator: 'a', textAwesome: 'awesome', textCrappy: 'crappy' }]}
-        />
-      </Provider>,
+    const wrapper = shallow(
+      <ClientSessionPage
+        classes={{ header: '', main: '' }}
+        cards={[{ indicator: 'a', textAwesome: 'awesome', textCrappy: 'crappy' }]}
+      />,
     );
 
     expect(wrapper.find(HealthIndicatorCard).length).toBe(1);
