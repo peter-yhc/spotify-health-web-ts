70ef8a7670854f251b7c171bc72ec9f8598d8439
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-06-02 23:39:28 +1000

    WIP

--- src/api/server-api.js
+++ src/api/server-api.js
@@ -1,6 +1,37 @@
+import io from 'socket.io-client';
+import axios from 'axios';
 import stub from '../heath-indicators/health-indicators-stub';
 
+// axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+let socket;
+
+const connectSocket = async () => {
+  socket = await io(':3000');
+  console.log('Socket Connected');
+};
+
 const createSession = async () => {
+  let response;
+  try {
+    console.log('creating session');
+    response = await axios({
+      method: 'POST',
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      url: 'http://localhost:3000/creator/new',
+      data: {
+        name: 'Session 1212',
+      },
+    });
+    console.log(response);
+    await connectSocket();
+  } catch (err) {
+    console.log(response);
+    console.log(err);
+  }
+
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(`http://${window.location.hostname}:${window.location.port}/clients?session=${Math.floor(Math.random() * 10000000000)}`);
