5f4f9adae56d29d0cd2ba76a9f382c7edeb14e57
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-06-06 15:14:34 +1000

    Remove localhost hardcoding.

--- src/api/server-api.js
+++ src/api/server-api.js
@@ -4,7 +4,7 @@ const createSession = async () => {
   try {
     const sessionId = (await axios({
       method: 'POST',
-      url: 'http://localhost:3001/creator/new',
+      url: `http://${window.location.hostname}:${3001}/creator/new`,
       data: {
         name: Math.random().toString(36).slice(2),
       },
@@ -12,7 +12,7 @@ const createSession = async () => {
 
     await axios({
       method: 'POST',
-      url: `http://localhost:3001/creator/${sessionId}/done`,
+      url: `http://${window.location.hostname}:${3001}/creator/${sessionId}/done`,
     });
 
     return sessionId;
@@ -24,13 +24,13 @@ const createSession = async () => {
 };
 
 const retrieveHealthIndicators = async (sessionId) => {
-  return (await axios.get(`http://localhost:3001/sessions/${sessionId}`)).data.indicators;
+  return (await axios.get(`http://${window.location.hostname}:${3001}/sessions/${sessionId}`)).data.indicators;
 };
 
 const registerClient = async (sessionId) => {
   const response = await axios({
     method: 'PUT',
-    url: `http://localhost:3001/sessions/${sessionId}/participants`,
+    url: `http://${window.location.hostname}:${3001}/sessions/${sessionId}/participants`,
     data: {
       name: Math.random().toString(36).slice(2),
     },
