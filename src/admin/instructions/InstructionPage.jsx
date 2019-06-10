8f36e8403a40c797b7b6d5a55275e4395ce5f7d0
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-06-10 11:45:54 +1000

    Simplify css and testing for InstructionPage component.

--- src/admin/instructions/InstructionPage.jsx
+++ src/admin/instructions/InstructionPage.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable max-len */
 import React from 'react';
-import { withStyles } from '@material-ui/styles';
-import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button/index';
 import { Link } from 'react-router-dom';
 import stubData from '../../health-indicators/health-indicators-stub';
 import { MiniHealthIndicatorCard } from '../../health-indicators';
 import Theme from '../../Theme';
 
-const styles = {
+const styles = makeStyles({
   container: {
     display: 'grid',
     gridTemplateColumns: '100px 3fr 2fr 100px',
@@ -27,26 +26,28 @@ const styles = {
     gridColumn: '2 / 3',
     padding: '0 2vw',
     textAlign: 'left',
-  },
-  heading: {
-    marginTop: '4vh',
-    marginBottom: '2vh',
-    '&:first-of-type': {
-      marginTop: 0,
-    },
-  },
-  text: {
     lineHeight: '28px',
+    '& p': {
+      marginBottom: '0.8rem',
+    },
+    '& h4': {
+      marginTop: '4vh',
+      marginBottom: '2vh',
+      '&:first-of-type': {
+        marginTop: 0,
+      },
+    },
   },
   startButton: {
     backgroundColor: Theme.GREEN_OVERRIDE,
     color: Theme.WHITE_OVERRIDE,
     marginTop: '3vh !important',
+    fontWeight: 500,
   },
-};
+});
 
-export const InstructionPage = (props) => {
-  const { classes } = props;
+const InstructionPage = () => {
+  const classes = styles();
 
   const showCards = () => {
     return stubData.map(data => (
@@ -64,42 +65,52 @@ export const InstructionPage = (props) => {
     <React.Fragment>
       <div className={classes.container}>
         <section className={classes.instructions}>
-          <h4 className={classes.heading}>How does this work?</h4>
-          <p className={classes.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec metus eget enim sollicitudin finibus
-            quis ut ligula. Nullam lacinia eu elit sit amet fringilla. In hac habitasse platea dictumst. Etiam euismod,
-            quam ac rhoncus feugiat, tortor est consectetur mauris, ac pharetra nisi nisl ac arcu. Sed rutrum risus
-            iaculis ipsum condimentum, ac molestie massa imperdiet. Ut molestie varius enim a porta. Quisque mi magna,
-            posuere vitae fringilla vel, feugiat ut libero. Phasellus feugiat augue vitae lacinia aliquet. Sed viverra
-            finibus nibh in tincidunt. Nulla eu aliquam mauris.
+          <h4>How does this work?</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
           </p>
-          <h4 className={classes.heading}>OK, but do models like this really work?</h4>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+          <h4>OK, but do models like this really work?</h4>
           <p className={classes.text}>
             Nullam gravida magna quis tellus auctor tincidunt. Aliquam erat volutpat. Donec vel sapien fringilla,
             egestas leo sit amet, dictum ligula. Morbi nec elit vitae ligula interdum posuere a in ante. Aliquam nec
-            nunc eget ligula sollicitudin posuere sed ac nisl. Fusce rhoncus accumsan turpis vitae mollis. Nam urna
+            nunc eget ligula sollicitudin posuere sed ac nisl.
+          </p>
+          <p>Fusce rhoncus accumsan turpis vitae mollis. Nam urna
             felis, pellentesque sit amet iaculis id, auctor eget velit. Suspendisse lectus magna, vestibulum ac sapien
             eget, consequat posuere justo. Cras elementum viverra convallis. Sed fringilla vestibulum tortor id commodo.
             Nulla elementum mi eget feugiat convallis. Morbi cursus augue eu diam pellentesque, eget hendrerit libero
-            tincidunt. Suspendisse porttitor arcu sit amet congue viverra. Morbi vel felis aliquam, euismod nisi at,
+            tincidunt. Suspendisse porttitor arcu sit amet congue viverra.
+          </p>
+          <p>Morbi vel felis aliquam, euismod nisi at,
             sollicitudin nisi. Duis tincidunt dui eu nunc posuere, at vehicula mi egestas.
           </p>
-          <h4 className={classes.heading}>Who the health check model is for</h4>
+          <h4>Who the health check model is for</h4>
           <p className={classes.text}>
             Fusce sapien est, malesuada eget scelerisque sit amet, mollis id turpis. Mauris luctus a nulla eget
             lobortis. Ut non lacus nisl. Etiam lacinia lobortis justo, eu sollicitudin nisl vulputate at. Nam justo
-            tellus, malesuada facilisis mollis a, mattis ac magna. Phasellus velit quam, mattis commodo vulputate a,
+            tellus, malesuada facilisis mollis a, mattis ac magna.
+          </p>
+          <p>Phasellus velit quam, mattis commodo vulputate a,
             ultricies eu nisi. Aenean ullamcorper metus arcu, id congue massa vulputate non.
           </p>
-          <h4 className={classes.heading}>How we do this</h4>
+          <h4>How we do this</h4>
           <p className={classes.text}>
             Donec a erat vulputate, bibendum nunc at, commodo orci. Mauris finibus interdum risus. Duis efficitur tellus
             sed dui mattis iaculis ut eget orci. Morbi eu sem nisi. Fusce ac turpis et metus dignissim facilisis.
-            Praesent faucibus nisi at vehicula iaculis. Phasellus vel erat eget mi maximus molestie vel et magna.
+            Praesent faucibus nisi at vehicula iaculis.
+          </p>
+          <p>Phasellus vel erat eget mi maximus molestie vel et magna.
             Maecenas sagittis dui eu lectus mollis rutrum.
           </p>
           <Link to="/admin/voting">
-            <Button className={classes.startButton} variant="contained">Lets get started</Button>
+            <Button className={classes.startButton} variant="contained" href="#">Get started</Button>
           </Link>
         </section>
         <section className={classes.cardContainer}>
@@ -110,8 +121,4 @@ export const InstructionPage = (props) => {
   );
 };
 
-InstructionPage.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(InstructionPage);
+export default InstructionPage;
--- src/admin/instructions/InstructionPage.test.js
+++ src/admin/instructions/InstructionPage.test.js
@@ -1,32 +1,27 @@
 import React from 'react';
-import { mount } from 'enzyme/build';
+import { shallow } from 'enzyme/build';
 import { Button } from '@material-ui/core/index';
 import { Link } from 'react-router-dom';
-import { MemoryRouter } from 'react-router';
-import SelectionMenu from './InstructionPage';
+import InstructionPage from './InstructionPage';
 import { MiniHealthIndicatorCard } from '../../health-indicators';
 
 describe('InstructionPage tests', () => {
   test('can show mini cards', () => {
-    const wrapper = mount(
-      <MemoryRouter>
-        <SelectionMenu />
-      </MemoryRouter>,
+    const wrapper = shallow(
+      <InstructionPage />,
     );
 
     expect(wrapper.find(MiniHealthIndicatorCard).length).toBe(10);
   });
 
   test('can show getting started button with link', () => {
-    const wrapper = mount(
-      <MemoryRouter>
-        <SelectionMenu />
-      </MemoryRouter>,
+    const wrapper = shallow(
+      <InstructionPage />,
     );
 
     const link = wrapper.find(Link);
     expect(link.props().to).toBe('/admin/voting');
     const button = link.find(Button);
-    expect(button.text()).toBe('Lets get started');
+    expect(button.text()).toBe('Get started');
   });
 });
